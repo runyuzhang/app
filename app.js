a43ece0160820d8aafacc7aaf5d0b2a31489bbe7
runyuzhang <runyuzhang@berkeley.edu>
2014-04-05 19:25:11 -0700

    2nd

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 College = new Meteor.Collection("colleges")
+Personal = new Meteor.Collection("attributes")
 
 if (Meteor.isClient) {
   Template.drink.greeting = function () {
-    return "Welcome to app.";
+    return "Drink up!";
   }
 
-  Template.drink.colleges = function() {
+  Template.drink.user = function() {
+    console.log("hello world");
+    return Meteor.user().emails[0].address();
+  }
+
+  Template.stats.colleges = function() {
     return College.find({});
   }
 
   Template.drink.events({
     'click #Berkeley': function () {
+
       College.update({_id:College.findOne({name:'Berkeley'})['_id']}, {$inc: {donation: 1}});
     },
     'click #Stanford': function () {
       College.update({_id:College.findOne({name:'Stanford'})['_id']}, {$inc: {donation: 1}});
+    },
+    'click': function () {
+      Personal.update({_id:Meteor.user()._id},{$inc: {donation: 1}}, {upsert:true});
     }
   });
 }
@@ -27,3 +37,11 @@ if (Meteor.isServer) {
     }
   });
 }
+
+Router.map(function() {
+  this.route('drink')
+  this.route('stats')
+  this.route('personal')
+});
+
+
